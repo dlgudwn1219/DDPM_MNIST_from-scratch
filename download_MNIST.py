590b1c686233c5dd45f905c20b9de55b8b543b50
dlgudwn1219 <dlgudwn1219@gmail.com>
2025-10-22 21:08:23 +0900

    mnist_plot function implemented

--- download_MNIST.py
+++ download_MNIST.py
@@ -1,7 +1,9 @@
 import torch
 from torchvision import datasets, transforms
+from utils.plot_mnist import plot_mnist
 import openml
 import os
+import numpy as np
 
 # List of Datasets in openml
 # df = openml.datasets.list_datasets(output_format="dataframe")
@@ -10,16 +12,14 @@ import os
 print("Current working directory: ", os.getcwd())
 
 # Openml: Platform for sharing datasets, algorithms
-openml.config.cache_directory = './openml'
-print(f"Cache dir: {os.path.abspath(openml.config.cache_directory)}")
-print("ENV OPENML_CACHE_DIR: ", os.environ.get("OPENML_CACHE_DIR"))
+# openml.config.cache_directory = './openml'
+# print(f"Cache dir: {os.path.abspath(openml.config.cache_directory)}")
+# print("ENV OPENML_CACHE_DIR: ", os.environ.get("OPENML_CACHE_DIR"))
 
 mnist = openml.datasets.get_dataset(554)
 
-print(mnist.name)
-print(mnist.version)
-print(mnist.default_target_attribute)
-print(mnist.format)
-
 X, y, _, _ = mnist.get_data(target = mnist.default_target_attribute)
 print(f"X: {X.shape}, y: {y.shape}")
+
+for i in range(10):
+    plot_mnist(np.array(X.iloc[i]), y[i], "images", f"image_{i}")
---
+++ utils/plot_mnist.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def plot_mnist(x: np.ndarray, y: int, folder: str, file_name: str):
+
+    assert isinstance(x, np.ndarray), "x must be "
+    assert x.shape == (784,), f"Expected shape (784,), got {x.shape}"
+
+    if not file_name.endswith(".png"):
+        file_name += ".png"
+
+    folder = os.path.join(os.getcwd(), folder)
+    os.makedirs(folder, exist_ok = True)
+
+    img = x.reshape(28, 28)
+    plt.imshow(img, cmap = "gray")
+    plt.axis("off")
+    plt.title(f"Class: {y}")
+
+    save_path = os.path.join(folder, file_name)
+    plt.savefig(save_path, bbox_inches = "tight", pad_inches = 0.1)
+    plt.close()
+
+    print(f"[INFO] Mnist sample saved to {save_path}!")
+
+    return
