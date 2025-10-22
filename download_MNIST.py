02c89f2fd677e3ceea2dbb48e9e2f05bce436db1
dlgudwn1219 <dlgudwn1219@gmail.com>
2025-10-22 21:43:11 +0900

    Download & Save & Load done

--- download_MNIST.py
+++ download_MNIST.py
@@ -5,21 +5,22 @@ import openml
 import os
 import numpy as np
 
-# List of Datasets in openml
-# df = openml.datasets.list_datasets(output_format="dataframe")
-# print(df.head())
-# os.environ["OPENML_CACHE_DIR"] = os.path.abspath("./openml")
-print("Current working directory: ", os.getcwd())
-
-# Openml: Platform for sharing datasets, algorithms
-# openml.config.cache_directory = './openml'
-# print(f"Cache dir: {os.path.abspath(openml.config.cache_directory)}")
-# print("ENV OPENML_CACHE_DIR: ", os.environ.get("OPENML_CACHE_DIR"))
-
 mnist = openml.datasets.get_dataset(554)
 
 X, y, _, _ = mnist.get_data(target = mnist.default_target_attribute)
+X, y = np.array(X, dtype = np.int64), np.array(y, dtype = np.int64)
+
 print(f"X: {X.shape}, y: {y.shape}")
 
-for i in range(10):
-    plot_mnist(np.array(X.iloc[i]), y[i], "images", f"image_{i}")
+# save in MNIST file
+os.makedirs("data", exist_ok = True)
+np.savez("data/mnist_cached.npz", feature=X, label=y)
+
+# Load & Plot data
+mnist_data = np.load("data/mnist_cached.npz")
+X, y = mnist_data["feature"], mnist_data["label"]
+
+for i in range(3):
+    plot_mnist(np.array(X[i]), y[i], "images", f"image_{i}")
+
+print("[Info] Download, Load, Plot all done!!")
